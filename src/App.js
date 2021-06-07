a535b7536b9e11d439249995f60e28a7878dc20c
Sergio C. Ochoa Rodriguez <sergioochoa@cbit-online.com>
2021-06-07 14:41:52 -0500

    Readme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import './Theme/theme.scss'
+
+
+export const baseURL = process.env.REACT_APP_TITLE;
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>{baseURL}<code> src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="fulanito"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
