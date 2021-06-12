9a57a4a2a7d80ff67a5e22a46cf7be26bbbe296c
Sergio C. Ochoa Rodriguez <sergioochoa@cbit-online.com>
2021-06-12 00:16:23 -0500

    buscador funcional

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 4000;
@@ -8,27 +9,82 @@ app.use(cors())
 app.use(express.static(__dirname + '/public'));
 
 
-const products = [
-    {
-        "author": {
-            "name": String,
-            "lastname": String
-        },
-        categories: [String, String, String],
-        item: {
-            "id": String,
-            "title": String,
-            "price": {
-                "currency": String,
-                "amount": Number,
-                "decimals": Number,
-            },
-            "picture": String,
-            "condition": String,
-            "free_shipping": Boolean,
-        }
-    }
-];
+app.get("/api/items?:query", (req, res) =>{
+    const url = `https://api.mercadolibre.com/sites/MLA/search?${req._parsedOriginalUrl.query}`;
+    axios.get(url).then(response => {
+        const apiMercadolibre = response.data.results;
+        [apiMercadolibre].map(function(obj) {
+            const products = [
+                {
+                    "autor": {
+                        name: "sergio",
+                        lastname: "ochoa"
+                    },
+                    description: [String, String, String],
+                    item: [
+                        {
+                            "id": obj[0].id,
+                            "title": obj[0].title,
+                            "price": {
+                                "currency": obj[0].currency_id,
+                                "amount": obj[0].price,
+                                "decimals": obj[0].installments.amount,
+                            },
+                            "picture": obj[0].thumbnail,
+                            "condition": obj[0].condition,
+                            "free_shipping": obj[0].shipping.free_shipping,
+                        },
+                        {
+                            "id": obj[1].id,
+                            "title": obj[1].title,
+                            "price": {
+                                "currency": obj[1].currency_id,
+                                "amount": obj[1].price,
+                                "decimals": obj[1].installments.amount,
+                            },
+                            "picture": obj[1].thumbnail,
+                            "condition": obj[1].condition,
+                            "free_shipping": obj[1].shipping.free_shipping,
+                        },
+                        {
+                            "id": obj[2].id,
+                            "title": obj[2].title,
+                            "price": {
+                                "currency": obj[2].currency_id,
+                                "amount": obj[2].price,
+                                "decimals": obj[2].installments.amount,
+                            },
+                            "picture": obj[2].thumbnail,
+                            "condition": obj[2].condition,
+                            "free_shipping": obj[2].shipping.free_shipping,
+                        },
+                        {
+                            "id": obj[3].id,
+                            "title": obj[3].title,
+                            "price": {
+                                "currency": obj[3].currency_id,
+                                "amount": obj[3].price,
+                                "decimals": obj[3].installments.amount,
+                            },
+                            "picture": obj[3].thumbnail,
+                            "condition": obj[3].condition,
+                            "free_shipping": obj[3].shipping.free_shipping,
+                        }
+                    ]
+                }
+            ]
+            res.json( {
+                products: products
+            })
+        });
+    }).catch(error => {
+
+    })
+})
+
+
+/*[0-4]*/
+
 
 const description = [
   {
@@ -55,27 +111,16 @@ const description = [
 
 
 
-app.get('/api/items/:query', (req, res) => {
-    res.json({
-        products: products
-    })
-});
-
-
-
-
-/*app.get('/api/items/:query', (req, res) => {
-    res.send({
-        search: `${req.params.query}!}`
-    })
-});*/
-
 app.get('/api/description', (req, res) => {
   res.json({
     description: description
   })
 
 });
+
+
+
+
 
 app.listen(port, function () {
   console.log(`Corriendo en puerto ${port}`);
--- src/Components/items.js
+++ src/Components/items.js
@@ -13,7 +13,7 @@ class Items extends Component {
     constructor() {
         super();
         this.state = {
-            products: [],
+            products: []
         }
     }
 
@@ -21,48 +21,49 @@ class Items extends Component {
         const param = History.location.search.substring(3);
 
         axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${param}`).then(response => {
-            console.log(response)
-           this.setState({
+                    const apiMercadolibre = response.data.results;
+                    [apiMercadolibre].map(function(obj) {
+                        obj.map(function(obj) {
+                            /*console.log( obj)*/
+                        });
+                    });
 
-               post: response.data
-           })
-        }).catch(error => {
+                    console.log(response.status)
 
-        })
+                }).catch(error => {})
         request
             .get(`http://localhost:4000/api/items?q=${param}`)
             .end((err, res) => {
-                const products = JSON.parse(res.text);
-                /*console.log(products.products[0].item.currency);*/
+                const products = JSON.parse(res.text).products;
+                /*console.log(products);*/
                 this.setState({
-                    name: products.products[0].item.currency = "perro"
+                    products: products[0].item
                 });
             });
     }
 
-
     render() {
-        var items = this.state.products.map((res, i) => {
-            return <>
-            <div className="products_item" key={i} rel={res.id}>
-                <div className="products_left">
-                    <img src={res.picture} className="products_portrait" alt="portrait product"/>
-                    <div className="products_box-text">
-                        <h3 className="products_title">$ {res.price.currency} <img className="products_add-on" src={Addon} alt="portrait product"/>
-                        </h3>
-                        <p className="products_description">{res.title}<br/> completo
-                            Unico!</p>
+        var iter = this.state.products.map((res, i) => {
+            console.log(res)
+            return     <div className="products_item" key={i} rel={res.id}>
+
+                    <div className="products_left">
+                        <img src={res.picture} className="products_portrait" alt="portrait product"/>
+                        <div className="products_box-text">
+                            <h3 className="products_title">$ {res.price.amount} <img  hidden={!res.free_shipping} className="products_add-on" src={Addon} alt="portrait product"/>
+                            </h3>
+                            <p className="products_description">{res.title}<br/> completo
+                                Unico!</p>
+                        </div>
+                    </div>
+                    <div className="products_right">
+                        <span className="products_tag">{res.condition}</span>
                     </div>
                 </div>
-                <div className="products_right">
-                    <span className="products_tag">{res.condition}</span>
-                </div>
-            </div>
-            </>
         });
         return (
             <div className="products">
-                {items}
+                {iter}
             </div>
         )
     }
