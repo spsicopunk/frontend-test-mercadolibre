8bbdfcf4d9522d272ce4b31e8704e63b80ee58c8
Sergio C. Ochoa Rodriguez <sergioochoa@cbit-online.com>
2021-06-14 02:58:29 -0500

    Heroku sama x

--- server.js
+++ server.js
@@ -12,86 +12,84 @@ app.use(express.static(__dirname + '/public'));
 
 /*creación de endpoint para busqueda de productos*/
 
-app.get("/api/items?:query", (req, res) =>{
+app.get("/api/items?:query", async (req, res) =>{
     const url = `https://api.mercadolibre.com/sites/MLA/search?${req._parsedOriginalUrl.query}`;
-    (async () =>
-         axios.get(url).then(response => {
-            const apiMercadolibre = response.data.results;
-            [apiMercadolibre].map(function(obj) {
-                const num0 = 0;
-                const num1 = 1;
-                const num2 = 2;
-                const num3 = 3;
+     axios.get(url).then(response => {
+        const apiMercadolibre = response.data.results;
+        [apiMercadolibre].map(function(obj) {
+            const num0 = 0;
+            const num1 = 1;
+            const num2 = 2;
+            const num3 = 3;
 
-                const products = [
-                    {
-                        "autor": {
-                            name: "sergio",
-                            lastname: "ochoa"
+            const products = [
+                {
+                    "autor": {
+                        name: "sergio",
+                        lastname: "ochoa"
+                    },
+                    description: [String, String, String],
+                    item: [
+                        {
+                            "id": obj[num0].id,
+                            "title": obj[num0].title,
+                            "price": {
+                                "currency": obj[num0].currency_id,
+                                "amount": obj[num0].price,
+                                "decimals": obj[num0].installments.amount
+                            },
+                            "picture": obj[num0].thumbnail,
+                            "condition": obj[num0].condition,
+                            "free_shipping": obj[num0].shipping.free_shipping
                         },
-                        description: [String, String, String],
-                        item: [
-                            {
-                                "id": obj[num0].id,
-                                "title": obj[num0].title,
-                                "price": {
-                                    "currency": obj[num0].currency_id,
-                                    "amount": obj[num0].price,
-                                    "decimals": obj[num0].installments.amount
-                                },
-                                "picture": obj[num0].thumbnail,
-                                "condition": obj[num0].condition,
-                                "free_shipping": obj[num0].shipping.free_shipping
+                        {
+                            "id": obj[num1].id,
+                            "title": obj[num1].title,
+                            "price": {
+                                "currency": obj[num1].currency_id,
+                                "amount": obj[num1].price,
+                                "decimals": obj[num1].installments.amount
                             },
-                            {
-                                "id": obj[num1].id,
-                                "title": obj[num1].title,
-                                "price": {
-                                    "currency": obj[num1].currency_id,
-                                    "amount": obj[num1].price,
-                                    "decimals": obj[num1].installments.amount
-                                },
-                                "picture": obj[num1].thumbnail,
-                                "condition": obj[num1].condition,
-                                "free_shipping": obj[num1].shipping.free_shipping
+                            "picture": obj[num1].thumbnail,
+                            "condition": obj[num1].condition,
+                            "free_shipping": obj[num1].shipping.free_shipping
+                        },
+                        {
+                            "id": obj[num2].id,
+                            "title": obj[num2].title,
+                            "price": {
+                                "currency": obj[num2].currency_id,
+                                "amount": obj[num2].price,
+                                "decimals": obj[num2].installments.amount
                             },
-                            {
-                                "id": obj[num2].id,
-                                "title": obj[num2].title,
-                                "price": {
-                                    "currency": obj[num2].currency_id,
-                                    "amount": obj[num2].price,
-                                    "decimals": obj[num2].installments.amount
-                                },
-                                "picture": obj[num2].thumbnail,
-                                "condition": obj[num2].condition,
-                                "free_shipping": obj[num2].shipping.free_shipping
+                            "picture": obj[num2].thumbnail,
+                            "condition": obj[num2].condition,
+                            "free_shipping": obj[num2].shipping.free_shipping
+                        },
+                        {
+                            "id": obj[num3].id,
+                            "title": obj[num3].title,
+                            "price": {
+                                "currency": obj[num3].currency_id,
+                                "amount": obj[num3].price,
+                                "decimals": obj[num3].installments.amount
                             },
-                            {
-                                "id": obj[num3].id,
-                                "title": obj[num3].title,
-                                "price": {
-                                    "currency": obj[num3].currency_id,
-                                    "amount": obj[num3].price,
-                                    "decimals": obj[num3].installments.amount
-                                },
-                                "picture": obj[num3].thumbnail,
-                                "condition": obj[num3].condition,
-                                "free_shipping": obj[num3].shipping.free_shipping
-                            }
-                        ]
-                    }
-                ]
+                            "picture": obj[num3].thumbnail,
+                            "condition": obj[num3].condition,
+                            "free_shipping": obj[num3].shipping.free_shipping
+                        }
+                    ]
+                }
+            ]
 
-                res.json( {
-                    products: products
-                })
-                return products;
-            });
-        }).catch(error => {
+            res.json( {
+                products: products
+            })
+            return products;
+        });
+    }).catch(error => {
 
-        })
-    )();
+    })
 })
 
 /*creación de endpoint para detalle de producto*/
